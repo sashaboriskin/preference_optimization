4f272d2de90b26022f4eab44cda55e882a3ed132
sashaboriskin <brskn.lx@gmail.com>
2024-12-21 14:24:12 +0300

    Add alignment parser

--- setup.bash
+++ setup.bash
@@ -6,3 +6,7 @@ pip install flash_attn==2.5.8 --force-reinstall -q
 pip install transformers==4.45.0 -q
 pip install huggingface-hub -U
 pip uninstall torchvision -y
+
+git clone https://github.com/huggingface/alignment-handbook.git
+cd ./alignment-handbook/
+python -m pip install .
