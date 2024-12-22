034e3ef14d1eb7e3d5ffddb71bb17fb74339c0a6
sashaboriskin <brskn.lx@gmail.com>
2024-12-22 15:19:45 +0300

    Update dpo config and run_script with dpo

--- run_script.bash
+++ run_script.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-new_model_name=Mistral-7B-Instruct-v0.3_bf16_simpo_v1
+new_model_name=Mistral-7B-Instruct-v0.3_bf16_dpo_v1
 model_id=mistralai/Mistral-7B-Instruct-v0.3
 
 pip install trl -U deepspeed
 echo "${PWD}"
 
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file ${PWD}/accelerate_configs/deepspeed_zero3.yaml ${PWD}/simpo.py --config ${PWD}/training_configs/simpo_v1.yaml
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file ${PWD}/accelerate_configs/deepspeed_zero3.yaml ${PWD}/dpo.py --config ${PWD}/training_configs/dpo_v1.yaml
 pip install peft -q
 
 python3 merge_lora.py \
